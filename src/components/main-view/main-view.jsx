426ddf49b80e029f621a75ab0ececa5b76eafdf2
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-09-24 20:00:55 -0400

    Installed and Imported axios into main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -16,16 +18,26 @@ export class MainView extends React.Component {
     };
   }
 
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
   render() {
     const { movies, selectedMovie } = this.state;
-  
-    if (selectedMovie) return <MovieView movie={selectedMovie} />;
-  
+
+
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-  
+
     return (
       <div className="main-view">
-        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          ))
+        }
       </div>
     );
   }
