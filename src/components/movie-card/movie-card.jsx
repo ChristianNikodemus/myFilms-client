040daba20127e4e801228b785c46d41da6598ef1
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-09-26 19:43:05 -0400

    Added prop types to movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -11,12 +11,15 @@ export class MovieCard extends React.Component {
   }
 }
 
-/*
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    ImagePath: PropTypes.string,
     Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired
+    Description: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    Genre: PropTypes.array.isRequired,
+    Director: PropTypes.array.isRequired,
+    Featured: PropTypes.bool.isRequired
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
-*/
